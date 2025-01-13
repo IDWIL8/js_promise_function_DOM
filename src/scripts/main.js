ece61d58ae698ce584a04aaee9ee230a8ab31f15
Inga <ingatkachuk8@gmail.com>
2025-01-13 17:15:40 +0500

    Test one

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,9 +5,8 @@ function waitFor(element, eventName) {
     element.addEventListener(
       eventName,
       () => {
-        resolve(
-          `It was ${eventName} on the element:${element.nodeName}, id: ${element.id}.`,
-        );
+        resolve(`It was ${eventName} on the element:
+      ${element.nodeName}, id: ${element.id}.`);
       },
       { once: true },
     );
